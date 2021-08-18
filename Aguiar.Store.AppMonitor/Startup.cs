1bdc75ad8a1ba6b046aff3a6a4ea889455a351a2
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2021-08-18 10:21:01 -0300

    app to monitoring health services

---
+++ Aguiar.Store.AppMonitor/Startup.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Aguiar.Store.AppMonitor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            // Configurando a verificação de disponibilidade de diferentes
+            // serviços através de Health Checks
+            services.AddHealthChecks()
+                .AddRabbitMQ(
+                    Configuration.GetConnectionString("RabbitMQ"),
+                    name: "RabbitMQ", tags: new string[] { "messaging" })
+                .AddSqlServer(
+                    Configuration.GetConnectionString("SqlServer"),
+                    name: "Sql Server", tags: new string[] { "db", "data" })
+                .AddMongoDb(
+                    Configuration.GetConnectionString("MongoDB"),
+                    name: "MongoDB", tags: new string[] { "db", "data" })
+                ;
+
+            services.AddHealthChecksUI()
+                .AddInMemoryStorage();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Gera o endpoint que retornará os dados utilizados no dashboard
+            app.UseHealthChecks("/healthchecks-data-ui", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            // Ativa o dashboard para a visualização da situação de cada Health Check
+            app.UseHealthChecksUI(options =>
+            {
+                options.UIPath = "/monitor";
+            });
+
+            // app.UseRouting();
+
+            // app.UseAuthorization();
+
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapControllers();
+            // });
+        }
+    }
+}
